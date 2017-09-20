daaacfc3294de4f112651e58fd59104c188107f5
danielrpg <daniel.fernandezgp@gmail.com>
2017-09-19 21:58:29 -0400

    Changes in client side added bootstrap

---
+++ searchAPI/src/main/java/api/search/engine/SearchApiEngineApplication.java
@@ -0,0 +1,12 @@
+package api.search.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApiEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApiEngineApplication.class, args);
+	}
+}
