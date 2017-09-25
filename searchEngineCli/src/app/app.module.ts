26e3df01e2451459658c6cec423b8218db8ce913
danielrpg <daniel.fernandezgp@gmail.com>
2017-09-24 23:23:35 -0400

    Cambios en la documentacion

--- searchEngineCli/src/app/app.module.ts
+++ searchEngineCli/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
