3beff945dea6a16d516fa23fdaeaa7acbacd4d13
danielrpg <daniel.fernandezgp@gmail.com>
2017-10-29 23:26:32 -0400

    Cambios en el documento principal de la Tesis

---
+++ searchapi2/src/main/java/com/search/api/SearchapiApplication.java
@@ -0,0 +1,12 @@
+package com.search.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchapiApplication.class, args);
+	}
+}
